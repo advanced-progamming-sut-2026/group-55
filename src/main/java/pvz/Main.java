eea7cc04ecadc99dbc11382b3102df0c1c0a4552
Amirmohammad <tavanaamirmohammad9@gmail.com>
2026-07-14 14:46:24 +0330

    add method takeDamage in Tile

--- src/main/java/pvz/Main.java
+++ src/main/java/pvz/Main.java
@@ -1,13 +1,16 @@
 package pvz;
 
 import pvz.model.core.Board;
-import pvz.model.core.Game;
+import pvz.model.core.Tile;
+import pvz.model.core.TileType;
 
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
-        System.out.println(board.getRows() + " x " + board.getCols());
-        System.out.println(board.getTile(2, 4).isPlantable());
-        System.out.println(board.inBounds(7, 3));
+        Tile test = new Tile(TileType.FROZEN);
+        test.takeDamage(400);
+        System.out.println(test.getType());
+        test.takeDamage(400);
+        System.out.println(test.getType());
     }
 }
