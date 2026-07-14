a0e3cae99065944657a57fe33e3f3369d6f060a8
Amirmohammad <tavanaamirmohammad9@gmail.com>
2026-07-14 08:39:40 +0330

    add tick engine

---
+++ src/main/java/pvz/model/core/Game.java
@@ -0,0 +1,26 @@
+package pvz.model.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private long currentTick = 0;
+    private final List<Updatable> updatables = new ArrayList<>();
+
+    public void register(Updatable u) {
+        updatables.add(u);
+    }
+
+    public void advance(int ticks) {
+        for (int i = 0; i < ticks; i++) {
+            currentTick++;
+            for (Updatable u : updatables) {
+                u.update(currentTick);
+            }
+        }
+    }
+
+    public long getCurrentTick() {
+        return currentTick;
+    }
+}
