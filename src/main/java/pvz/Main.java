ed09f12a83e8f06935829e4449a1465a0211653b
Amirmohammad <tavanaamirmohammad9@gmail.com>
2026-07-14 11:56:11 +0330

    add board and tyle Types

--- src/main/java/pvz/Main.java
+++ src/main/java/pvz/Main.java
@@ -1,11 +1,13 @@
 package pvz;
 
+import pvz.model.core.Board;
 import pvz.model.core.Game;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.register(tick -> System.out.println("tick: " + tick));
-        game.advance(5);
+        Board board = new Board();
+        System.out.println(board.getRows() + " x " + board.getCols());
+        System.out.println(board.getTile(2, 4).isPlantable());
+        System.out.println(board.inBounds(7, 3));
     }
 }
---
+++ src/main/java/pvz/model/core/TileType.java
@@ -0,0 +1,12 @@
+package pvz.model.core;
+
+public enum TileType {
+    NORMAL,
+    TOMBSTONE,
+    SLIPPERY_UP,
+    SLIPPERY_DOWN,
+    FROZEN,
+    WATER,
+    LOW_BEACH,
+    NECROMANCY
+}
